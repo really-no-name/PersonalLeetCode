e56fb7872cb7278a11bf1f0be27b03d396d4c0a1
really-no-name <xu_bolun@126.com>
2025-02-19 21:38:52 +0800

    1062-py-2

---
+++ Code/1064/1064_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+文件名: PersonalLeetCode
+作者: Bolun Xu
+创建日期: 2025
+版本: 1.0
+描述: 这个文件用于演示Python文件模板的常用结构。
+时间复杂度： O(N)
+空间复杂度： O(1)
+执行用时： 0 ms
+消耗内存： 18. 51 mb
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def fixedPoint(self, arr: List[int]) -> int:
+        for i in range(len(arr)):
+            if arr[i] == i:
+                return i
+        else:
+            return -1
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.fixedPoint(arr=[-10, -5, 0, 3, 7]))
