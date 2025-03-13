7daf5d5fdfa5e21846b07f288cbf252f2214d5b8
really-no-name <xu_bolun@126.com>
2025-03-13 15:58:19 +0800

    3306-py

---
+++ Code/3306/3306.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+文件名: PersonalLeetCode 3306.py
+作者: Bolun Xu
+创建日期: 2025/3/13
+版本: 1.0
+描述: 元音辅音字符串计数 II。
+时间复杂度：
+空间复杂度：
+"""
+
+
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.countOfSubstrings(word="aeioqq", k=1))
