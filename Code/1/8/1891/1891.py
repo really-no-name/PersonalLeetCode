f4462b34125f67691234f5e67c9ae47c98c046f1
really-no-name <xu_bolun@126.com>
2025-03-28 14:33:38 +0800

    1891-py

---
+++ Code/1/8/1891/1891.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+文件名: PersonalLeetCode 1891.py
+作者: Bolun Xu
+创建日期: 2025/3/28
+版本: 1.0 -- 开区间二分
+描述: 割绳子。
+时间复杂度：
+空间复杂度：
+"""
+from typing import List
+
+
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        if k > sum(ribbons):
+            return 0
+        elif k == sum(ribbons):
+            return 1
+
+        def check(x: int) -> bool:
+            s = 0
+            for ribbon in ribbons:
+                s += ribbon // x
+            return s >= k
+
+        left = 0
+        right = max(ribbons) + 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid
+        return left
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.maxLength(ribbons = [9,7,5], k = 3))
