f1b0fd9bdb2f49ce958a4cc76e5410cc4c310570
really-no-name <xu_bolun@126.com>
2025-04-10 09:30:14 +0800

    1876-py

---
+++ Code/1/8/1876/1876.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+文件名: PersonalLeetCode 1876.py
+作者: Bolun Xu
+创建日期: 2025/4/10
+版本: 1.0
+描述: 长度为三且各字符不同的子字符串。
+时间复杂度：
+空间复杂度：
+"""
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        s = list(s)
+        cnt = 0
+        for i in range(len(s) - 2):
+            print(s[i: i + 3])
+            if len(set(s[i: i + 3])) == 3:
+                cnt += 1
+        return cnt
