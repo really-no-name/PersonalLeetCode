fd47ca9106c366d8c51f60ed1c33f68b6a86c319
really-no-name <xu_bolun@126.com>
2025-04-11 09:58:52 +0800

    1929-py

---
+++ Code/1/9/1929/1929.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+文件名: PersonalLeetCode 1929.py
+作者: Bolun Xu
+创建日期: 2025/4/11
+版本: 1.0
+描述: 数组串联。
+时间复杂度： O(2N)
+空间复杂度： O(2N)
+"""
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
