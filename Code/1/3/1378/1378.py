cef02c36c0c909561a7acbf765a62b16fb8dbcb8
really-no-name <xu_bolun@126.com>
2025-04-22 10:42:18 +0800

    1378

--- Code/1/3/1378/1378.py
+++ Code/1/3/1378/1378.py
@@ -6,7 +6,10 @@
 作者: Bolun Xu
 创建日期: 2025/4/21
 版本: 1.0
-描述: 。
-时间复杂度：
-空间复杂度：
+描述: 使用唯一标识码替换员工ID。
 """
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    merge_df = pd.merge(employees, employee_uni, on='id', how='left')
+    return merge_df[['unique_id', 'name']]
