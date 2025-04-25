569b9d6138fec3c3650e1e0402ed45d97ada0554
really-no-name <xu_bolun@126.com>
2025-04-25 23:35:26 +0800

    550-py

---
+++ Code/0/5/0550/0550.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+文件名: PersonalLeetCode 0550.py
+作者: Bolun Xu
+创建日期: 2025/4/23
+版本: 1.0
+描述: 游戏玩法分析 IV。
+"""
+import pandas as pd
+
+
+def gameplay_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    # 根据玩家分组，找到每位玩家的第一次登录日期
+    first_login = activity.groupby('player_id')['event_date'].min()
+
+    # 计算每位玩家的第二次登录日期（第一次登录日期 + 1天）
+    second_login = first_login + pd.DateOffset(days=1)
+
+    # 将第二次登录日期与原数据表合并
+    merged_table = pd.merge(activity, second_login, on='player_id', how='left')
+
+    # 计算满足条件的行数：第二次登录日期等于实际登录日期的行数
+    matching_rows_count = len(merged_table.query('event_date_x == event_date_y'))
+
+    # 计算满足条件的玩家比例：满足条件的玩家数量 / 所有玩家数量
+    fraction = round(matching_rows_count / activity['player_id'].nunique(), 2)
+
+    # 创建结果 DataFrame 并返回
+    result_df = pd.DataFrame({'fraction': [fraction]})
+    return result_df
+
+
+if __name__ == '__main__':
+    data = [[1, 2, '2016-03-01', 5], [1, 2, '2016-03-02', 6], [2, 3, '2017-06-25', 1], [3, 1, '2016-03-02', 0],
+            [3, 4, '2018-07-03', 5]]
+    activity = pd.DataFrame(data, columns=['player_id', 'device_id', 'event_date', 'games_played']).astype(
+        {'player_id': 'Int64', 'device_id': 'Int64', 'event_date': 'datetime64[ns]', 'games_played': 'Int64'})
+    print(gameplay_analysis(activity))
